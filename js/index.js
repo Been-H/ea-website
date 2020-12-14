2f99a3a4fd63d31eb1925c8f4cb7ac8b001e4e0d
Been-H <71901668+Been-H@users.noreply.github.com>
2020-12-13 23:41:24 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,22 @@
+
+function dropdown() {
+  document.getElementById("dd").classList.toggle("show");
+
+}
+function dropdown1() {
+  document.getElementById("dd1").classList.toggle("show");
+}
+
+function remove() {
+  if (document.getElementById('dd').classList.contains("show")) {
+
+    document.getElementById('dd').classList.remove("show");
+
+  }
+  if (document.getElementById('dd1').classList.contains("show")) {
+    document.getElementById('dd1').classList.remove("show")
+  }
+}
+document.getElementById("click").addEventListener("click", dropdown);
+document.getElementById("click1").addEventListener("click", dropdown1)
+document.body.addEventListener("click", remove, true)
